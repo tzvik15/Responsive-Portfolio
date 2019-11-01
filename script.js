e2ff134e70f9d6ca2b2b9df4b766f94e864f3b45
tzvik15 <tzvik15@yahoo.com>
2019-11-01 14:15:54 -0600

    created and linked three remaining project pages

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $("#btn1").on("click", ()=>{
     window.location.href ="projects/bonez.html";
 })
+
+$("#btn2").on("click", ()=>{
+    window.location.href="projects/python.html";
+})
+
+$("#btn3").on("click", ()=>{
+    window.location.href="projects/weather.html";
+})
+
+$("#btn4").on("click", ()=>{
+    window.location.href="projects/bassoon.html";
+})
